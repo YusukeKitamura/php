9a6122c8a78ba943a557211acb184b56918adb17
北村裕介 <yk08072002@yahoo.co.jp>
2016-02-27 00:52:51 +0800

    計算を少し早くした

--- 素数/primes.php
+++ 素数/primes.php
@@ -1,4 +1,5 @@
 <?php 
+	//$n個の素数を見つける
 	function generate_primes($n) {
 		$prime = array(2);
 		$x = 1; 
@@ -6,10 +7,15 @@
 		while ($k < $n) {
 			$x += 2;
 			$j = 0;
-			while ($j < $k && $x % $prime[$j] != 0){
+			//while ($j < $k && $x % $prime[$j] != 0){
+			//ある数の平方根までで割り切れる素数が見つからなかったら終わり
+			$p = $prime[0];
+			while ($x >= $p*$p && $x % $p != 0){
 				$j++;
+				$p = $prime[$j];
 			}
-			if ($j == $k) {
+			//if ($j == $k) {
+			if ($x < $p*$p) {
 				$prime[] = $x;
 				$k++;
 			}
@@ -20,10 +26,10 @@
 	$output = '';
 	if(isset($_POST) && !empty($_POST)){
 	    $n = (int)htmlspecialchars($_POST['sosuu']);
-	    if ($n > 0 && $n <= 10000) {
+	    if ($n > 0 && $n <= 1000000) {
 			$output = generate_primes($n);
 		} else {
-			echo '1から10000までの整数を入力してください';
+			echo '1から1000000までの整数を入力してください';
 		}
 	}
 
